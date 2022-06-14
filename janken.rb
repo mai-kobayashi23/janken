5904c2fa5c5195571874e8af72f6fae0fcee594d
mai-kobayashi23 <mai0323.k@gmail.com>
2022-06-14 19:52:05 +0900

    ジャンケンプログラムを修正し、エラーなく動くプログラムにすることができました

--- janken.rb
+++ janken.rb
@@ -35,7 +35,7 @@ def janken
 end
 
 def validation 
-    if @player_hand != "g" || @player_hand != "c" || @player_hand != "p"
+    if @player_hand != "g" && @player_hand != "c" && @player_hand != "p"
         puts 'もう一度入力してください (press g or c or p)'
         player_hand = gets.chomp
         @player_hand = player_hand
@@ -52,6 +52,9 @@ def battle
 
     if @player_hand == @computer_hand
         puts "あいこで...(press g or c or p)"
+        player_hand = gets.chomp
+        @player_hand = player_hand
+        validation
         battle
     elsif (@player_hand == hand.keys[0] && @computer_hand == hand.keys[1]) || (@player_hand == hand.keys[1] && @computer_hand == hand.keys[2]) || (@player_hand == hand.keys[2] && @computer_hand == hand.keys[0])
         puts "勝ち！"
@@ -72,6 +75,5 @@ def result
         puts "#{@win_count}勝#{@lose_count}敗であなたの負け"
     end
 end
-
 
 janken
